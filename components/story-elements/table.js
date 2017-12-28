fc39f3eb46642a03460d686bd8bbfdc04af5883a
Ravigopal Kesari <kesariravigopal@gmail.com>
2017-12-28 16:34:49 +0530

    Sets data for table only when parsing is completed

--- components/story-elements/table.js
+++ components/story-elements/table.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import papa from 'papaparse';
-import ReactTable from 'react-table';
+import React from "react";
+import papa from "papaparse";
+import ReactTable from "react-table";
 import _ from "lodash";
 
 class Table extends React.Component {
@@ -13,41 +13,49 @@ class Table extends React.Component {
   }
 
   parseCSVToJson(content) {
-    const parsedContent = papa.parse(content,{ header: this.props.hasHeader });
-    this.setState({tableData: parsedContent.data});
+    papa.parse(content, {
+      header: this.props.hasHeader,
+      complete: results => this.setState({ tableData: results.data })
+    });
   }
 
   componentDidMount() {
     this.parseCSVToJson(this.props.data.content);
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (this.props.data.content !== nextProps.data.content) {
+      this.parseCSVToJson(nextProps.data.content);
+    }
+  }
+
   render() {
-    if(!this.state.tableData.length > 0) {
+    if (!this.state.tableData.length > 0) {
       return null;
     }
 
     const columns = _.chain(this.state.tableData)
-                      .first()
-                      .keys()
-                      .map(header => ({
-                         Header: header,
-                         accessor: header,
-                         headerStyle: !this.props.hasHeader ? { display: 'none' } : {}
-                        }))
-                      .value();
+      .first()
+      .keys()
+      .map(header => ({
+        Header: header,
+        accessor: header,
+        headerStyle: !this.props.hasHeader ? { display: "none" } : {}
+      }))
+      .value();
 
     const className = `story-element-table-${this.props.id}`;
 
     return (
-      <ReactTable data={this.state.tableData}
-                  columns={columns}
-                  showPageSizeOptions={false}
-                  showPageJump={false}
-                  className={className}
+      <ReactTable
+        data={this.state.tableData}
+        columns={columns}
+        showPageSizeOptions={false}
+        showPageJump={false}
+        className={className}
       />
     );
   }
 }
-
 
 export default Table;
