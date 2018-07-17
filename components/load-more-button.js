163efc62330b09c0bba3825505c095940de871f1
Garima Kamboj <garima.kamboj@icloud.com>
2018-07-17 12:09:45 +0530

    Remove styleName from load-more button

--- components/load-more-button.js
+++ components/load-more-button.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function LoadMoreButton(props) {
-    return <button styleName={props.styleName} onClick={props.onLoadMore}>Load more</button>
+    return <button onClick={props.onLoadMore}>Load more</button>
 }
 
 export { LoadMoreButton };
