df1ab5046531f7f9e6131bf7f0eba1e8da268b21
nandap4790 <nandap4790@gmail.com>
2021-03-02 12:54:44 +0530

    revert redirect url

--- src/components/social-logins/with-social-login.js
+++ src/components/social-logins/with-social-login.js
@@ -6,15 +6,15 @@ import { postRequest } from '../api-client';
  * This is an abstract render props component for logging in with social accounts. The component adds two items to scope: `serverSideLoginPath` for redirecting to server side, and `login` for doing a client side login. Calling `login()` returns a promise which can be used to handle success and failure cases.
  *
  * This component should not be used directly, but should be used via one of the scomponents mentioned in the example.
- * 
+ *
  * Please contact the support in order to update the social app credentials in the Bridgekeeper database after making the relevant code changes.
  *
  * NOTE:
  * - Twitter and LinkedIn do not support ClientSideLogin, and thus `login()` will just redirect to the server. It also ignores the apiKey
  * - Twitter and LinkedIn do not verify presence of email on the client side. Please ask for these permissions in the app
  * - The `login()` need not be called when serverSideLoginPath is called
- * 
- * 
+ *
+ *
  * Example
  * ```javascript
  * import { WithFacebookLogin, WithGoogleLogin, WithTwitterLogin, WithLinkedInLogin } from '@quintype/components';
@@ -52,7 +52,7 @@ import { postRequest } from '../api-client';
 export class WithSocialLogin extends React.Component {
   constructor(props) {
     super(props);
-    this.bridgekeeperServerLoginPath = `/api/auth/v1/login?auth-provider=${this.props.provider}&redirect-url=${this.props.redirectUrl}`;
+    this.bridgekeeperServerLoginPath = `/api/auth/v1/login?auth-provider=${this.props.provider}`;
     this.defaultServerLoginPath = `/login?auth-provider=${this.props.provider}&remote-host=${global.location && global.location.origin}`;
     this.serverLoginPath = this.props.isBridgekeeperLogin ? this.bridgekeeperServerLoginPath : this.defaultServerLoginPath;
     this.serverSideSSOLoginPath = `/login?auth-provider=${this.props.provider}&redirect-url=${this.props.sso && this.props.redirectUrl ? this.props.redirectUrl : global.location && global.location.origin}`;
