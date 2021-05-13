8ec5a493fca7bbf65dc26748ba2e4536a071e83e
ags1773 <amogh@quintype.com>
2021-05-13 15:41:48 +0530

    fix: linting | add defensive check in collectionToStories

--- src/components/wrap-collection-layout.js
+++ src/components/wrap-collection-layout.js
@@ -1,39 +1,51 @@
-import React from 'react';
-import {connect} from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
 import get from "lodash/get";
 
-import { LoadMoreCollectionStories } from './load-more-collection-stories';
-import { LazyLoadImages } from './lazy-load-images';
-import { ClientSideOnly } from './client-side-only';
+import { LoadMoreCollectionStories } from "./load-more-collection-stories";
+import { LazyLoadImages } from "./lazy-load-images";
+import { ClientSideOnly } from "./client-side-only";
 
-function loadMoreWrapper(Component, data, enableLoadMoreButton, slug, numStoriesToLoad) {
-  return !enableLoadMoreButton ?
-    React.createElement(Component, data) :
-    React.createElement(LoadMoreCollectionStories, {
-      template: Component,
-      collectionSlug: slug,
-      params: { 'item-type': 'story' },
-      data: data,
-      numStoriesToLoad,
-    });
+function loadMoreWrapper(
+  Component,
+  data,
+  enableLoadMoreButton,
+  slug,
+  numStoriesToLoad
+) {
+  return !enableLoadMoreButton
+    ? React.createElement(Component, data)
+    : React.createElement(LoadMoreCollectionStories, {
+        template: Component,
+        collectionSlug: slug,
+        params: { "item-type": "story" },
+        data: data,
+        numStoriesToLoad,
+      });
 }
 
-function lazyLoadWrapper(component, {lazy_load_images: lazyLoadImages = false}) {
-  return !lazyLoadImages ?
-    component :
-    React.createElement(LazyLoadImages, {}, component);
+function lazyLoadWrapper(
+  component,
+  { lazy_load_images: lazyLoadImages = false }
+) {
+  return !lazyLoadImages
+    ? component
+    : React.createElement(LazyLoadImages, {}, component);
 }
 
-function clientSideLoadWrapper(component, {client_side_only: clientSideOnly = false}) {
-  return !clientSideOnly ?
-    component :
-    React.createElement(ClientSideOnly, {}, component);
+function clientSideLoadWrapper(
+  component,
+  { client_side_only: clientSideOnly = false }
+) {
+  return !clientSideOnly
+    ? component
+    : React.createElement(ClientSideOnly, {}, component);
 }
 
 function WrapCollectionComponent(Component) {
-  return function(props) {
+  return function (props) {
     if (!props.collection) {
-      return <div></div>
+      return <div></div>;
     }
 
     const associatedMetadata = props.collection["associated-metadata"] || {};
@@ -47,10 +59,20 @@ function WrapCollectionComponent(Component) {
       associatedMetadata: associatedMetadata,
     });
 
-    const component = loadMoreWrapper(Component, data, associatedMetadata.enable_load_more_button, props.collection.slug, associatedMetadata.subsequent_stories_load_count || 10);
+    const component = loadMoreWrapper(
+      Component,
+      data,
+      associatedMetadata.enable_load_more_button,
+      props.collection.slug,
+      associatedMetadata.subsequent_stories_load_count || 10
+    );
 
-    return [clientSideLoadWrapper, lazyLoadWrapper].reduce((accumulator, currentElement) => currentElement(accumulator, associatedMetadata), component);
-  }
+    return [clientSideLoadWrapper, lazyLoadWrapper].reduce(
+      (accumulator, currentElement) =>
+        currentElement(accumulator, associatedMetadata),
+      component
+    );
+  };
 }
 
 /**
@@ -71,42 +93,53 @@ function WrapCollectionComponent(Component) {
  * @returns {Component} A component which can be passed collection
  */
 export function wrapCollectionLayout(component) {
-  const wrappedComponent = connect((state) => ({config: state.qt.config}))(WrapCollectionComponent(component));
-  if(component.storyLimit) {
+  const wrappedComponent = connect((state) => ({ config: state.qt.config }))(
+    WrapCollectionComponent(component)
+  );
+  if (component.storyLimit) {
     wrappedComponent.storyLimit = component.storyLimit;
   }
 
-  if(component.nestedCollectionLimit) {
+  if (component.nestedCollectionLimit) {
     wrappedComponent.nestedCollectionLimit = component.nestedCollectionLimit;
   }
-  
+
   return wrappedComponent;
 }
 
 /**
  * Use this function to get the stories from a collection object
+ * returns null if collection doesn't have items key
  *
  * @param {Collection} collection
  * @category Collection Page
  * @returns {Array} An array of stories
  */
 export function collectionToStories(collection) {
+  if (!collection.items) return null;
   return collection.items
-                   .filter(item => item.type === 'story')
-                   .map(item => replaceWithAlternates(item.story));
+    .filter((item) => item.type === "story")
+    .map((item) => replaceWithAlternates(item.story));
 }
 
 function replaceWithAlternates(story) {
   const alternates = get(story, ["alternative", "home", "default"]);
 
-  if(!alternates)
-    return story;
+  if (!alternates) return story;
 
   return Object.assign({}, story, {
-    headline                 : alternates.headline || story.headline,
-    "hero-image-s3-key"      : alternates["hero-image"] ? alternates["hero-image"]["hero-image-s3-key"] : story["hero-image-s3-key"],
-    "hero-image-metadata"    : alternates["hero-image"] ? alternates["hero-image"]["hero-image-metadata"] : story["hero-image-metadata"],
-    "hero-image-caption"     : alternates["hero-image"] ? alternates["hero-image"]["hero-image-caption"] : story["hero-image-caption"],
-    "hero-image-attribution" : alternates["hero-image"] ? alternates["hero-image"]["hero-image-attribution"] : story["hero-image-attribution"],
-  })
+    headline: alternates.headline || story.headline,
+    "hero-image-s3-key": alternates["hero-image"]
+      ? alternates["hero-image"]["hero-image-s3-key"]
+      : story["hero-image-s3-key"],
+    "hero-image-metadata": alternates["hero-image"]
+      ? alternates["hero-image"]["hero-image-metadata"]
+      : story["hero-image-metadata"],
+    "hero-image-caption": alternates["hero-image"]
+      ? alternates["hero-image"]["hero-image-caption"]
+      : story["hero-image-caption"],
+    "hero-image-attribution": alternates["hero-image"]
+      ? alternates["hero-image"]["hero-image-attribution"]
+      : story["hero-image-attribution"],
+  });
 }
