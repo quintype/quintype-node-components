e2488e8cfa16b17c53204d556604c581885fcc5c
Bindiya <bindiya@quintype.com>
2021-09-15 19:06:46 +0530

    lazy laoding

--- src/components/story-elements/jsembed.js
+++ src/components/story-elements/jsembed.js
@@ -1,40 +1,39 @@
-import React from 'react';
-import atob from 'atob';
+import atob from "atob";
+import { PropTypes } from "prop-types";
+import React from "react";
+import { WithLazy } from "../with-lazy";
 
-function cloneScriptNode(node){
-  var script  = document.createElement("script");
+function cloneScriptNode(node) {
+  var script = document.createElement("script");
   script.text = node.innerHTML;
-  for (var i = node.attributes.length-1; i >= 0; i--) {
+  for (var i = node.attributes.length - 1; i >= 0; i--) {
     script.setAttribute(node.attributes[i].name, node.attributes[i].value);
   }
   return script;
 }
 
 function replaceScriptNodes(node) {
-  if (node.tagName === 'SCRIPT') {
+  if (node.tagName === "SCRIPT") {
     node.parentNode.replaceChild(cloneScriptNode(node), node);
-    if (window.instgrm) window.instgrm.Embeds.process(); //Temporary fix for instagram element on live blog page.
+    if (window.instgrm) window.instgrm.Embeds.process(); // Temporary fix for instagram element on live blog page.
   } else {
     var i = 0;
     var children = node.childNodes;
-    while ( i < children.length ) {
+    while (i < children.length) {
       replaceScriptNodes(children[i++]);
     }
   }
 }
 
-export default class JSEmbed extends React.Component {
+class CustomJSEmbed extends React.Component {
   shouldComponentUpdate(nextProps) {
-    return !(
-      this.props.id === nextProps.id &&
-      this.props.embedJS === nextProps.embedJS
-    )
+    return !(this.props.id === nextProps.id && this.props.embedJS === nextProps.embedJS);
   }
 
   constructor(props) {
     super(props);
 
-    this.uniqueId = 'anagh';
+    this.uniqueId = "anagh";
   }
 
   componentDidMount() {
@@ -51,6 +50,24 @@ export default class JSEmbed extends React.Component {
   }
 
   render() {
-    return <div ref={(jsembed) => { this.JSEmbed = jsembed; }} dangerouslySetInnerHTML={{__html: this.getEmbedJS()}} />
+    return (
+      <div
+        ref={jsembed => {
+          this.JSEmbed = jsembed;
+        }}
+        dangerouslySetInnerHTML={{ __html: this.getEmbedJS() }}
+      />
+    );
   }
 }
+
+CustomJSEmbed.propTypes = {
+  id: PropTypes.string,
+  embedJS: PropTypes.string
+};
+
+const JSEmbed = props => {
+  return <WithLazy margin="0px">{() => <CustomJSEmbed {...props} />}</WithLazy>;
+};
+
+export default JSEmbed;
