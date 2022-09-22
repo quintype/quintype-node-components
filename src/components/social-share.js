b3b05527f2f8f8a0ed7804fc97fbbcf3fccbba0c
phaneendra <58028287+cricket-lover@users.noreply.github.com>
2022-09-22 17:42:43 +0530

    change facebook share url format

--- src/components/social-share.js
+++ src/components/social-share.js
@@ -1,6 +1,6 @@
 import React from "react";
-import {connect} from "react-redux";
-import {withError} from './with-error';
+import { connect } from "react-redux";
+import { withError } from "./with-error";
 
 function getNativeShareHandler(canNativeShare, title, fullUrl) {
   if (!canNativeShare) {
@@ -8,44 +8,59 @@ function getNativeShareHandler(canNativeShare, title, fullUrl) {
   }
 
   return function handleShare() {
-    window.navigator.share({
-      title: title,
-      url: fullUrl,
-    }).catch(console.error);
-  }
+    window.navigator
+      .share({
+        title: title,
+        url: fullUrl,
+      })
+      .catch(console.error);
+  };
 }
 
 class SocialShareBase extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      canNativeShare: false
-    }
+      canNativeShare: false,
+    };
   }
-
   componentDidMount() {
-    this.setState({canNativeShare: global && global.navigator && global.navigator.share})
+    this.setState({ canNativeShare: global && global.navigator && global.navigator.share });
   }
 
   render() {
     const fullUrl = this.props.fullUrl || `${this.props.publisherUrl}/${this.props.url}`;
-    const hashtags = this.props.hashtags ? this.props.hashtags : '';
+    const hashtags = this.props.hashtags ? this.props.hashtags : "";
 
-    return React.createElement(this.props.template, Object.assign({
-      fbUrl: `https://www.facebook.com/sharer.php?u=${encodeURIComponent(fullUrl)}`,
-      twitterUrl: `https://twitter.com/intent/tweet?url=${encodeURIComponent(fullUrl)}&text=${encodeURIComponent(this.props.title)}&hashtags=${hashtags}`,
-      gplusUrl: `https://plus.google.com/share?url=${encodeURIComponent(fullUrl)}`,
-      linkedinUrl: `https://www.linkedin.com/shareArticle?url=${encodeURIComponent(fullUrl)}&title=${encodeURIComponent(this.props.title)}`,
-      whatsappUrl: `https://api.whatsapp.com/send?text=${encodeURIComponent(fullUrl)}`,
-      mailtoUrl: `mailto:${''}?subject=${encodeURIComponent(this.props.title)}&body=${encodeURIComponent(fullUrl)}`,
-      handleNativeShare: getNativeShareHandler(this.state.canNativeShare, this.props.title, fullUrl)
-    }, this.props));
+    console.log("Full Url is --->", this.props.app_id, "props --->", this.props);
+
+    return React.createElement(
+      this.props.template,
+      Object.assign(
+        {
+          fbUrl: `https://www.facebook.com/dialog/share?app_id=${this.props.app_id}&href=${encodeURIComponent(
+            fullUrl
+          )}redirect_uri=${encodeURIComponent(fullUrl)}`,
+          twitterUrl: `https://twitter.com/intent/tweet?url=${encodeURIComponent(fullUrl)}&text=${encodeURIComponent(
+            this.props.title
+          )}&hashtags=${hashtags}`,
+          gplusUrl: `https://plus.google.com/share?url=${encodeURIComponent(fullUrl)}`,
+          linkedinUrl: `https://www.linkedin.com/shareArticle?url=${encodeURIComponent(
+            fullUrl
+          )}&title=${encodeURIComponent(this.props.title)}`,
+          whatsappUrl: `https://api.whatsapp.com/send?text=${encodeURIComponent(fullUrl)}`,
+          mailtoUrl: `mailto:${""}?subject=${encodeURIComponent(this.props.title)}&body=${encodeURIComponent(fullUrl)}`,
+          handleNativeShare: getNativeShareHandler(this.state.canNativeShare, this.props.title, fullUrl),
+        },
+        this.props
+      )
+    );
   }
 }
 
 function mapStateToProps(state) {
   return {
-    publisherUrl: state.qt.config["sketches-host"]
+    publisherUrl: state.qt.config["sketches-host"],
   };
 }
 
@@ -59,17 +74,17 @@ function mapStateToProps(state) {
  * class CustomComponent extends React.Component {
  *   getSocialCardsTemplate({fbUrl, twitterUrl, gplusUrl, linkedinUrl, handleNativeShare}) {
  *     return <ul className="social-share-icons">
-*        <li className="social-share-icon">
-*          <a href={fbUrl} target="_blank">
-*            <img src={fbIcon} alt="fb icon"/>
-*          </a>
-*        </li>
-*        {handleNativeShare && <li className="social-share-icon">
-*          <button onClick={handleNativeShare}>
-*            <img src={fbIcon} alt="share icon"/>
-*          </button>
-*        </li>}
-*      </ul>
+ *        <li className="social-share-icon">
+ *          <a href={fbUrl} target="_blank">
+ *            <img src={fbIcon} alt="fb icon"/>
+ *          </a>
+ *        </li>
+ *        {handleNativeShare && <li className="social-share-icon">
+ *          <button onClick={handleNativeShare}>
+ *            <img src={fbIcon} alt="share icon"/>
+ *          </button>
+ *        </li>}
+ *      </ul>
  *   }
  *
  *   render() {
