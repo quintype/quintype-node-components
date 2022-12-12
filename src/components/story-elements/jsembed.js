710e04830a7ac6a280fcd5d4b20d298fcf74e340
jeevan-kishore <kishor080@gmail.com>
2022-12-12 11:18:12 +0530

    feat(atob): Decode using utf 8

--- src/components/story-elements/jsembed.js
+++ src/components/story-elements/jsembed.js
@@ -1,5 +1,6 @@
 import { string } from "prop-types";
 import React from "react";
+import atob from "atob-utf-8";
 
 function cloneScriptNode(node) {
   var script = document.createElement("script");
@@ -44,11 +45,7 @@ export default class JSEmbed extends React.Component {
 
   getEmbedJS() {
     const embedJs = this.props.embedJS;
-    if (!embedJs) return null;
-    if (global) {
-      return decodeURIComponent(escape(global.atob(embedJs)));
-    }
-    return Buffer.from(embedJs, "base64").toString("utf-8");
+    return embedjs != null ? atob(embedjs) : null;
   }
 
   render() {
