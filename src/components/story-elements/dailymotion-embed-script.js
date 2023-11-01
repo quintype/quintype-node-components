7a88016d38ede36a1ba9338eb80424eae81304bb
Vishwanath <90732088+vishu2222@users.noreply.github.com>
2023-11-02 01:19:34 +0530

    fix dm embed

--- src/components/story-elements/dailymotion-embed-script.js
+++ src/components/story-elements/dailymotion-embed-script.js
@@ -5,8 +5,11 @@ class DailyMotion extends React.Component {
   constructor(props) {
     super(props);
     this.containerRef = React.createRef();
+    this.observerRef = React.createRef();
     this.state = {
       showVideo: false,
+      scriptLoaded: false,
+      dmPlayer: null,
     };
   }
 
@@ -22,12 +25,48 @@ class DailyMotion extends React.Component {
     });
   };
 
+  handleIntersection = (entries) => {
+    if (entries?.[0].isIntersecting) this.state.dmPlayer.play();
+    else this.state.dmPlayer.pause();
+  };
+
+  startObserver() {
+    this.intersectionObserver = new IntersectionObserver(this.handleIntersection, {
+      threshold: 0.75,
+    });
+    this.intersectionObserver.observe(this.observerRef.current);
+  }
+
+  componentDidUpdate() {
+    if (!this.observerRef.current) return;
+    if (!this.state.dmPlayer) return;
+    if (!this.state.scriptLoaded) return;
+    this.state.dmPlayer.on("start", () => this.startObserver());
+  }
+
+  componentWillUnmount() {
+    if (this.observerRef.current) this.intersectionObserver.disconnect();
+  }
+
   addScript = () => {
     const { "video-id": videoId, "player-id": playerId } = this.props.element.metadata;
     const script = document.createElement("script");
     script.src = `https://geo.dailymotion.com/player/${playerId}.js`;
     script.dataset.video = videoId;
     this.containerRef.current.appendChild(script);
+
+    script.onload = () => {
+      this.setState({ scriptLoaded: true });
+      window.dailymotion
+        .createPlayer(this.containerRef.current.id, {
+          video: videoId,
+          player: playerId,
+        })
+        .then((player) => {
+          this.setState({ dmPlayer: player });
+        })
+        .catch((e) => console.error("dm--error", e));
+    };
   };
 
   render() {
@@ -47,12 +86,26 @@ class DailyMotion extends React.Component {
             </div>
           )}
           {this.state.showVideo && (
-            <div className="dailymotion-embed-wrapper dm-embed-integration" ref={this.containerRef} />
+            <div ref={this.observerRef}>
+              <div
+                className="dailymotion-embed-wrapper dm-embed-integration"
+                ref={this.containerRef}
+                id={`dm-embed-container-id-${videoId}`}
+              />
+            </div>
           )}
         </>
       );
     } else if (!this.props.loadIframeOnClick) {
-      return <div className="dailymotion-embed-wrapper dm-embed-integration" ref={this.containerRef} />;
+      return (
+        <div ref={this.observerRef}>
+          <div
+            className="dailymotion-embed-wrapper dm-embed-integration"
+            ref={this.containerRef}
+            id={`dm-embed-container-id-${videoId}`}
+          />
+        </div>
+      );
     }
   }
 }
